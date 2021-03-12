c381da4a6b1b409ccf9bb5af2fa99e456dca7ab5
wusi <wusi@WudeMacBook-Air.local>
2021-03-12 22:09:23 +0800

    succeed in coordinate setting of slam map in Plan2D
    1. "thinginToken" and "thinginEndpoint" work in new version of Plan2D, no need to login Thingin portal and the cookies
    2. "thinginToken" can be retrieve by ThinginAdaper api: post /auth with fixed basic token of the developer account, like si.wu@orange.com
    3. in the setting of blueprint:
    "scale": the pixels/m, it should be equal to 1.0/map.yaml.resolution
    "indoorLocation"."origin": it should be equal to "x": map.yaml.origin[0]/map.yaml.resolution, "y": map.yaml.origin[1]/map.yaml.resolution

--- Plan2D/js/test.js
+++ Plan2D/js/test.js
@@ -5,15 +5,16 @@ var graphData = {
     "legend": false,
     "toolbar": true,
     "backgroundColor": "lightgray",
-    // "thinginToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJlMjU5M2ZjYy04MmNhLTExZWItYjY3OC1mYTE2M2U5YjQyYzciLCJzdWIiOiJjOGIwNjc3Ny1lY2I5LTQ4Y2YtOTA3Yy1jMDEyMzI5ZDc3Y2MiLCJleHRlcm5hbGlkIjoic2kud3VAb3JhbmdlLmNvbSIsImlzcyI6IlRoaW5nX2luIiwiaWF0IjoxNjE1NTA5MzQ1LCJleHAiOjE2MTU1OTU3NDUsImRvbWFpbnMiOlsiaHR0cDovLyIsImh0dHBzOi8vIl0sInNjb3BlcyI6WyJwcm92aWRlciJdfQ.WIz5DrFaUrd8GwXGNbF1EMkIZyw9Ai99eJAIXkkb59bOckv_v8eziVHACCpMy37ovqEqN_XJkUkykOirna3GKd8OkjPEspW4_tpb5vvmHFxkWI5gacaG3ll834KzISpBsP9sqGkOET3wN4EIWuSQn178pVYYyZkVaciMj93Sp2dAnCGnOJrw0hGWNnctAmj8lh7ky0ptN_dam5Wichh9-GHu3E5Wkbck-ByFR7TYsi-rSozeiOOZoh14l5MTy_c36jgZDyzYdNQAvxFZkur6x6KikENkOmlYqMoG1i6ZyWCCIZx7HzTCpXv-ooB_PY9GClZqJKwlfsjEDJAH3ioKGQ",
-    // "thinginEndpoint": "https://coreapi.thinginthefuture.com/",
+    // "thinginToken" can be retrieve by ThinginAdaper api: post /auth with fixed basic token of the developer account, like si.wu@orange.com 
+    "thinginToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJlMjU5M2ZjYy04MmNhLTExZWItYjY3OC1mYTE2M2U5YjQyYzciLCJzdWIiOiJjOGIwNjc3Ny1lY2I5LTQ4Y2YtOTA3Yy1jMDEyMzI5ZDc3Y2MiLCJleHRlcm5hbGlkIjoic2kud3VAb3JhbmdlLmNvbSIsImlzcyI6IlRoaW5nX2luIiwiaWF0IjoxNjE1NTA5MzQ1LCJleHAiOjE2MTU1OTU3NDUsImRvbWFpbnMiOlsiaHR0cDovLyIsImh0dHBzOi8vIl0sInNjb3BlcyI6WyJwcm92aWRlciJdfQ.WIz5DrFaUrd8GwXGNbF1EMkIZyw9Ai99eJAIXkkb59bOckv_v8eziVHACCpMy37ovqEqN_XJkUkykOirna3GKd8OkjPEspW4_tpb5vvmHFxkWI5gacaG3ll834KzISpBsP9sqGkOET3wN4EIWuSQn178pVYYyZkVaciMj93Sp2dAnCGnOJrw0hGWNnctAmj8lh7ky0ptN_dam5Wichh9-GHu3E5Wkbck-ByFR7TYsi-rSozeiOOZoh14l5MTy_c36jgZDyzYdNQAvxFZkur6x6KikENkOmlYqMoG1i6ZyWCCIZx7HzTCpXv-ooB_PY9GClZqJKwlfsjEDJAH3ioKGQ",
+    "thinginEndpoint": "https://coreapi.thinginthefuture.com/",
     "niceNameFields": ['http://www.w3.org/2006/vcard/ns#given-name','http://www.w3.org/2006/vcard/ns#street-address', "http://www.w3.org/ns/td#id"],
     "blueprint": {
       "url" : "http://localhost:5500/assets/map2.png",         // image url
       "id": "map2",                   // id of svg element in file
-      "scale": 20,  // pixels/m
+      "scale": 20,  // pixels/m  it should be equal to 1.0/map.yaml.resolution
       "indoorLocation":{
-         "origin": {"x": -50, "y": -50},
+         "origin": {"x": 51.224998*20, "y": 51.224998*20},  //it should be equal to "x": map.yaml.origin[0]/map.yaml.resolution, "y": map.yaml.origin[1]/map.yaml.resolution
          "rotation" : 0
       },
       "ifc" : {
